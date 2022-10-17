16c32be111ef3c4eec28a207fbf336f481221b5f
mproios <mproios@maximgrp.com>
2022-10-17 01:42:38 -0400

    changes

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ import NotFound from "app/components/NotFound";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "PulseTrail",
+  title: "PulseTrail Cloud",
   viewport: "width=device-width,initial-scale=1",
 });
 
